b4342e59b8256c2eea1846576b81b60f2613c8e9
prasadkatkar <prasadkatkar07@gmail.com>
2026-01-15 13:21:06 +0530

    Responsive navbar design done

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -5,11 +5,17 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { deleteCookie } from "cookies-next";
+import { TfiAlignLeft } from "react-icons/tfi";
 
 export function Profile() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [open, setOpen] = useState(false);
   const router = useRouter();
+
+  function handleOpen() {
+    setOpen(!open);
+  }
 
   async function handleLogout() {
     localStorage.removeItem("jwt");
@@ -43,14 +49,38 @@ export function Profile() {
   }, []);
 
   return (
-    <div className="bg-gray-800 p-3 flex justify-between items-center">
-      <div className="flex gap-10">
+    <div className="md:bg-amber-800 bg-gray-800 p-3 flex md:justify-between items-center w-full sticky top-0 gap-3">
+      <div className="md:hidden flex justify-between w-full">
+        <Button onClick={handleOpen}>
+          <TfiAlignLeft />
+        </Button>
+        <Button onClick={handleLogout}>Logout</Button>
+      </div>
+      {open && (
+        <div className="absolute top-16 left-0 bg-gray-800 w-full md:hidden flex flex-col gap-4 p-4">
+          <h1>Name : {name}</h1>
+          <p>Email : {email}</p>
+
+          <Link href={"/"} onClick={() => setOpen(false)}>
+            Home
+          </Link>
+          <Link href={"/profile"} onClick={() => setOpen(false)}>
+            Profile
+          </Link>
+          <Link href={"/create"} onClick={() => setOpen(false)}>
+            Create Todo
+          </Link>
+        </div>
+      )}
+
+      <div className="hidden md:flex gap-10">
         <Link href={"/"}>Home</Link>
         <Link href={"/profile"}>Profile</Link>
         <Link href={"/create"}>Create Todo</Link>
       </div>
-      <div className="flex gap-5 items-center">
-        <h1>User Name : {name}</h1>
+
+      <div className="hidden md:flex gap-5 items-center">
+        <h1>Name : {name}</h1>
         <p>Email : {email}</p>
         <div>
           <Button variant="ghost" onClick={handleLogout}>
